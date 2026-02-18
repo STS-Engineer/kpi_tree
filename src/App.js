38d24deefecd0e8c5f3879e08db78a9e82f3f085
STS-Engineer <administration.sts@avocarbon.com>
2026-02-18 12:20:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 // src/App.js
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import PlantTreeNavigatorEnhanced from './Components/PlantTreeNavigatorEnhanced';
+import PlantStatsDashboard from './Components/PLantstatsdashboard';
 
 
 function App() {
   return (
- 
-      <Router>
-        <div className="App">
-  
-          <Routes>
-          
-        
-             <Route path="/" element={<PlantTreeNavigatorEnhanced/>} />
-            {/* Catch all route */}
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </div>
-      </Router>
- 
+
+    <Router>
+      <div className="App">
+
+        <Routes>
+
+
+          <Route path="/" element={<PlantTreeNavigatorEnhanced />} />
+          <Route path="/dashboard" element={<PlantStatsDashboard />} />
+          {/* Catch all route */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
+    </Router>
+
   );
 }
 
